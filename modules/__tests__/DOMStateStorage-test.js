5d2ea73d5e6a09ba053b110f9c442d612b6ba926
Andreas Köhler <andi5.py@gmx.net>
2016-12-21 10:00:51 +0100

    Fix sessionStorage use for browsers with disabled cookies. Fixes #397
    On multiple browsers, accessing window.sessionStorage with disabled cookies
    will throw an exception. So even checking that value against undefined (for IE
    when served via file protocol) must happen within a try-catch-block.

--- modules/__tests__/DOMStateStorage-test.js
+++ modules/__tests__/DOMStateStorage-test.js
@@ -14,4 +14,33 @@ describe('dom state storage', () => {
 
     expect(readState('key3')).toBe(undefined)
   })
+
+  it('does not throw without sessionStorage', () => {
+    const previousSessionStorage = window.sessionStorage
+    try {
+      delete window.sessionStorage
+      saveState('key1', {id: 1})
+      readState('key1')
+    } finally {
+      window.sessionStorage = previousSessionStorage
+    }
+  })
+
+  it('does not throw when sessionStorage is not available due to security settings', () => {
+    const previousSessionStorage = window.sessionStorage
+    try {
+      Object.defineProperty(window, 'sessionStorage', {
+        get: () => {
+          const securityError = new Error('broken')
+          securityError.name = 'SecurityError'
+          throw securityError
+        }
+      })
+      saveState('key1', {id: 1})
+      readState('key1')
+    } finally {
+      delete window.sessionStorage
+      window.sessionStorage = previousSessionStorage
+    }
+  })
 })
